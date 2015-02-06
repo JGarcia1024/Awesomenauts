df967c15637f16ef4001baaf936f66938197ec2b
Joshua Garcia <j.garcia12998@gmail.com>
2015-02-05 22:54:12 -0800

    Video 19 No Comments

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -242,15 +242,36 @@ game.EnemyCreep = me.Entity.extend({
 		this.health = 10;
 		this.alwaysUpdate = true;
 		//sets speed
-		this.setVelocity(3, 20);
+		this.body.setVelocity(3, 20);
 
 		this.type = "EnemyCreep";
 		//uses images
-		this,renderable.addAnimation("walk", [3, 4, 5], 80);
+		this.renderable.addAnimation("walk", [3, 4, 5], 80);
 		this.renderable.setCurrentAnimation("walk");
 	},
 
 	update: function(){
 
+	}
+});
+
+game.GameManager = Object.extend({
+	init: function(x, y, settings){
+		this.now = new Date().getTime();
+		this.lastCreep = new Date().getTime();
+
+		this.alwaysUpdate = true;
+	},
+
+	update: function(){
+		this.now = new Date().getTime();
+
+		if(Math.round(this.now/1000)%10 ===0 && (this.now - this.lastCreep >= 1000)){
+			this.lastCreep = this.now;
+			var creep = me.pool.pull("EnemyCreep", 1000, 0, {});
+			me.game.world.addChild(creepe, 5);
+		}
+
+		return true;
 	}
 });
--- js/screens/play.js
+++ js/screens/play.js
@@ -10,6 +10,9 @@ game.PlayScreen = me.ScreenObject.extend({
 
 		var player = me.pool.pull("player", 0 ,420, {});
 		me.game.world.addChild(player, 5);
+
+		var gamemanager = me.pool.pull("GameManager", 0, 0, {});
+		me.game.world.addChild(gamemanager, 0);
 		//sets right key to move player right
 		me.input.bindKey(me.input.KEY.RIGHT, "right");
 		//sets left key to move player left
