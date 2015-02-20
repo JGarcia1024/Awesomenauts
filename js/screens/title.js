f0aae70fddff7391ba3b56eb2df8e7676b5a5b3a
Joshua Garcia <j.garcia12998@gmail.com>
2015-02-19 23:57:17 -0800

    Video 27 Comments

--- js/screens/title.js
+++ js/screens/title.js
@@ -5,15 +5,15 @@ game.TitleScreen = me.ScreenObject.extend({
 	onResetEvent: function() {	
 		//adds title screen
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10); // TODO
-		
+		//makes ENTER key work
 		me.input.bindKey(me.input.KEY.ENTER, "start");
-
+		//chooses where words spawn
 		me.game.world.addChild(new (me.Renderable.extend({
 			init: function(){
 				this._super(me.Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.height]);
 				this.font = new me.Font("Arial", 46, "white");
 			},
-
+		//adds text
 			draw: function(renderer){
 				this.font.draw(renderer.getContext(), "Awesomenauts!", 450, 130);
 				this.font.draw(renderer.getContext(), "Press ENTER to play!", 250, 530);
@@ -34,6 +34,7 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
+		//unbinds ENTER key after title is loaded
 		me.input.unbindKey(me.input.KEY.ENTER); // TODO
 		me.event.unsubscribe(this.handler);
 	}
