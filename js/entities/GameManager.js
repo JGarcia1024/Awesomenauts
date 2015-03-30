f60bf79e531cf164b75a7d2c8302c0803ae979a4
Joshua Garcia <j.garcia12998@gmail.com>
2015-03-30 02:26:44 -0700

    Video 50

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -122,6 +122,9 @@ game.SpendGold = Object.extend({
 			}
 		}
 
+		//checks the buy keys below
+		this.checkBuyKeys();
+
 
 		return true;
 		
@@ -161,12 +164,12 @@ game.SpendGold = Object.extend({
 			//function renderer tells the font size of the words in quotations
 			draw: function(renderer){
 				this.font.draw(renderer.getContext(), "PRESS F1-F6 TO BUY, B TO EXIT. Current Gold: " + game.data.gold, this.pos.x, this.pos.y);
-				this.font.draw(renderer.getContext(), "Skill 1: Increase Damage. Current Level: " + game.data.skill1 + " Cost: " + ((game.data.exp1+1)*10);
-				this.font.draw(renderer.getContext(), "Skill 2: Run Faster! Current Level: " + game.data.skill2 + " Cost: " + ((game.data.exp2+1)*10), this.pos.x, this.pos.y);
-				this.font.draw(renderer.getContext(), "Skill 3: Increase Health. Current Level:  " + game.data.skill3 + " Cost: " + ((game.data.exp1+1)*10), this.pos.x, this.pos.y);
-				this.font.draw(renderer.getContext(), "O Ability: Speed Burst. Current Level: " + game.data.ability1 + " Cost: " + ((game.data.ability4+1)*10), this.pos.y);
-				this.font.draw(renderer.getContext(), "W Ability: Eat Your Creep for Health: " + game.data.ability2 + " Cost: " + ((game.data.ability5+1)*10), this.pos.y);
-				this.font.draw(renderer.getContext(), "E Ability: Throw Your Spear: " + game.data.ability3 + " Cost: " + ((game.data.ability6+1)*10), this.pos.y);
+				this.font.draw(renderer.getContext(), "Skill 1: Increase Damage. Current Level: " + game.data.skill1 + " Cost: " + ((game.data.skill1+1)*10), this.pos.x, this.pos.y);
+				this.font.draw(renderer.getContext(), "Skill 2: Run Faster! Current Level: " + game.data.skill2 + " Cost: " + ((game.data.skill2+1)*10), this.pos.x, this.pos.y);
+				this.font.draw(renderer.getContext(), "Skill 3: Increase Health. Current Level:  " + game.data.skill3 + " Cost: " + ((game.data.skill3+1)*10), this.pos.x, this.pos.y);
+				this.font.draw(renderer.getContext(), "O Ability: Speed Burst. Current Level: " + game.data.ability1 + " Cost: " + ((game.data.ability1+1)*10), this.pos.y);
+				this.font.draw(renderer.getContext(), "W Ability: Eat Your Creep for Health: " + game.data.ability2 + " Cost: " + ((game.data.ability2+1)*10), this.pos.y);
+				this.font.draw(renderer.getContext(), "E Ability: Throw Your Spear: " + game.data.ability3 + " Cost: " + ((game.data.ability3+1)*10), this.pos.x, this.pos.y);
 
 			}
 
@@ -190,5 +193,96 @@ game.SpendGold = Object.extend({
 		me.game.world.removeChild(game.data.buytext);
 	}
 
+//check that the buy keys are working
+checkBuyKeys: function(){
+	if(me.input.isKeyPressed("F1")){
+		//checks the cost of the key pressed
+		if(this.checkCost(1)){
+			//checks if the player bought the upgrade
+			this.makePurchase(1);
+		}
+	}else if(me.input.isKeyPressed("F2")){
+		//checks the cost of the key pressed
+		if(this.checkCost(2)){
+			//checks if the player bought the upgrade
+			this.makePurchase(2);
+		}
+	}else if(me.input.isKeyPressed("F3")){
+		//checks the cost of the key pressed
+		if(this.checkCost(3)){
+			//checks if the player bought the upgrade
+			this.makePurchase(3);
+		}
+	}else if(me.input.isKeyPressed("F4")){
+		//checks the cost of the key pressed
+		if(this.checkCost(4)){
+			//checks if the player bought the upgrade
+			this.makePurchase(4);
+		}
+	}else if(me.input.isKeyPressed("F5")){
+		//checks the cost of the key pressed
+		if(this.checkCost(5)){
+			//checks if the player bought the upgrade
+			this.makePurchase(5);
+		}
+	}else if(me.input.isKeyPressed("F6")){
+		//checks the cost of the key pressed
+		if(this.checkCost(6)){
+			//checks if the player bought the upgrade
+			this.makePurchase(6);
+		}
+	}
+},
+//checks the cost of the key pressed
+checkCost: function(skill){
+	//if the key is pressed and if the player has the money the skill will be achieved
+	if(skill===1 && (game.data.gold >= ((game.data.skill1+1)*10))){
+		return true;
+	}//if the key is pressed and if the player has the money the skill will be achieved
+	else if(skill===2 && (game.data.gold >= ((game.data.skill2+1)*10))){
+		return true;
+	}//if the key is pressed and if the player has the money the skill will be achieved
+	else if(skill===3 && (game.data.gold >= ((game.data.skill3+1)*10))){
+		return true;
+	}//if the key is pressed and if the player has the money the skill will be achieved
+	else if(skill===4 && (game.data.gold >= ((game.data.ability1+1)*10))){
+		return true;
+	}//if the key is pressed and if the player has the money the skill will be achieved
+	else if(skill===5 && (game.data.gold >= ((game.data.ability2+1)*10))){
+		return true;
+	}//if the key is pressed and if the player has the money the skill will be achieved
+	else if(skill===6 && (game.data.gold >= ((game.data.ability3+1)*10))){
+		return true;
+		//if the player does not have the money they will not get the skill
+	}else{
+		return false;
+	}
+},
+//enables the purchase to be made
+makePurchase: function(skill){
+	if(skill === 1){
+		game.data.gold -= ((game.data.skill1 + 1)* 10);
+		game.data.skill += 1;
+		game.data.player.attack += 1;
+	}else if(skill ===2){
+		game.data.gold -= ((game.data.skill2 + 1)* 10);
+		game.data.skill2 += 1;
+	}else if(skill ===3){
+		game.data.gold -= ((game.data.skill3 + 1)* 10);
+		game.data.skill3 += 1;
+	}else if(skill ===4){
+		game.data.gold -= ((game.data.ability1 + 1)* 10);
+		game.data.ability1 += 1;
+	}else if(skill ===5){
+		game.data.gold -= ((game.data.ability2 + 1)* 10);
+		game.data.ability2 += 1;
+	}else if(skill ===6){
+		game.data.gold -= ((game.data.ability3 + 1)* 10);
+		game.data.ability3 += 1;
+	}
+}
+
+
+
 });
-// 6:06
+
